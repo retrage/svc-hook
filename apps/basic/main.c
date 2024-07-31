a4344c23b02a83b7b9f34b14814b7f93dbc8f1ac
Akira Moroo <akiram@ricsec.co.jp>
2024-07-31 06:10:58 +0000

    apps/basic: Add basic hook application
    Signed-off-by: Akira Moroo <akiram@ricsec.co.jp>

---
+++ apps/basic/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef long (*syscall_fn_t)(long, long, long, long, long, long, long, long);
+
+static syscall_fn_t next_sys_call = NULL;
+
+static long hook_function(long a1, long a2, long a3, long a4, long a5, long a6,
+                          long a7, long a8) {
+  printf("output from hook_function: syscall number %ld\n", a7);
+  return next_sys_call(a1, a2, a3, a4, a5, a6, a7, a8);
+}
+
+int __hook_init(long placeholder __attribute__((unused)),
+                void *sys_call_hook_ptr) {
+  printf("output from __hook_init: we can do some init work here\n");
+
+  next_sys_call = *((syscall_fn_t *)sys_call_hook_ptr);
+  *((syscall_fn_t *)sys_call_hook_ptr) = hook_function;
+
+  return 0;
+}
