71c99b1e63949cce2b9fdaf51d673d73ac91a56b
Akira Moroo <retrage01@gmail.com>
2024-12-08 05:06:43 +0000

    main: Use dlmopen only if glibc is used
    Signed-off-by: Akira Moroo <retrage01@gmail.com>

--- main.c
+++ main.c
@@ -536,14 +536,13 @@ static void load_hook_lib(void) {
       return;
     }
 
-#ifdef __ANDROID__
-    // Android does not support dlmopen
-    handle = dlopen(filename, RTLD_NOW | RTLD_LOCAL);
-#else
+#ifdef __GLIBC__
     handle = dlmopen(LM_ID_NEWLM, filename, RTLD_NOW | RTLD_LOCAL);
+#else
+    handle = dlopen(filename, RTLD_NOW | RTLD_LOCAL);
 #endif
     if (!handle) {
-      fprintf(stderr, "dlmopen failed: %s\n\n", dlerror());
+      fprintf(stderr, "dlopen/dlmopen failed: %s\n\n", dlerror());
       fprintf(
           stderr,
           "NOTE: this may occur when the compilation of your hook function "
